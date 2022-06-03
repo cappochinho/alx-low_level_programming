f8cf3cfb0a3be1facb16f5cf6869dfbd3c14bac4
cappochinho <edwardprempeh18@gmail.com>
2022-06-02 19:07:13 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - short description
  * puts a string
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""\"Programming is like building a multilingual puzzle");
 	return (0);
-	/*returns 0*/
 }
