413e41767f0ab643ed8b6e56eb6d8895e85178fa
cappochinho <edwardprempeh18@gmail.com>
2022-06-02 18:56:38 -0700

    my first Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - short description
+ * puts a string
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+	/*returns 0*/
+}
