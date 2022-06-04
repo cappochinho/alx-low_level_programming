74fbc45cb762f798e0c016fc3a236856e10c09b3
cappochinho <edwardprempeh18@gmail.com>
2022-06-03 17:33:17 -0700

    Print lowrcas except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Start of execution
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s[30] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; s[i] != ('\0'); i++)
+	{
+		if (s[i] == 'q' || s[i] == 'e')
+			s[i];
+		else
+			putchar(s[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
