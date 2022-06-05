4a15519e50169c50028ca190b6150544334eb406
cappochinho <edwardprempeh18@gmail.com>
2022-06-04 19:21:42 -0700

    Prints double digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Program starts
+ * Prints the least of the combination of -
+ * two digits in ascending order of magnitude
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 57; i++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
