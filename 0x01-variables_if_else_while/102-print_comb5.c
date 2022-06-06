352bc5ffbd68f9dfa652374d06790f615b6d5bec
cappochinho <edwardprempeh18@gmail.com>
2022-06-05 17:04:38 -0700

    Two 2-digits combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Program starts
+ * Prints the least of the combination of -
+ * three digits in ascending order of magnitude
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int e, f, g;
+
+	for (e = 48; e <= 55; e++)
+	{
+		for (f = 49; f <= 56; f++)
+		{
+			for (g = 50; g <= 57; g++)
+			{
+				if ((e < f) && (f < g))
+				{
+					putchar(e);
+					putchar(f);
+					putchar(g);
+					if (e != 55 || f != 56 || g != 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
