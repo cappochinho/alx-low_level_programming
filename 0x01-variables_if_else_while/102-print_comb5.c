bc83ce0d6fc677df116e98ca1899195ca540900e
cappochinho <edwardprempeh18@gmail.com>
2022-06-05 17:16:01 -0700

    Two-digit combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,21 +14,22 @@ int main(void)
 	for (s = 0; s < 100; s++)
 	{
 	        for (t = 0; t < 100; t++)
-	        {
-                    if (s < t)
-                    {
-                            putchar((s/10) + 48);
-                            putchar((s%10) + 48);
-                            putchar(32);
-                            putchar((t/10) + 48);
-                            putchar((t%10) + 48);
-                            if (s != 98 || t != 99)
-                            {
-                                    putchar(44);
-                                    putchar(32);
-                            }
-                    }
-	        }
+		{
+			if (s < t)
+                    	{
+                        	putchar((s/10) + 48);
+				putchar((s%10) + 48);
+				putchar(32);
+				putchar((t/10) + 48);
+				putchar((t%10) + 48);
+				if (s != 98 || t != 99)
+ 				{
+					putchar(44);
+    					putchar(32);
+				}
+	    
+		    	}
+		}
 	}
 	
 	putchar('\n');
