5067a5ebb7f115aa01a1634580bca360851eb3df
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-06 00:23:37 +0000

    Two-digit combos

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,28 +10,27 @@
 int main(void)
 {
 	int s, t;
-	
+
 	for (s = 0; s < 100; s++)
 	{
-	        for (t = 0; t < 100; t++)
+		for (t = 0; t < 100; t++)
 		{
 			if (s < t)
-                    	{
-                        	putchar((s/10) + 48);
-				putchar((s%10) + 48);
+			{
+				putchar('0' + (s / 10));
+				putchar('0' + (s % 10));
 				putchar(32);
-				putchar((t/10) + 48);
-				putchar((t%10) + 48);
+				putchar('0' + (t / 10));
+				putchar('0' + (t % 10));
 				if (s != 98 || t != 99)
- 				{
+				{
 					putchar(44);
-    					putchar(32);
+					putchar(32);
 				}
-	    
-		    	}
+			}
+
 		}
 	}
-	
 	putchar('\n');
 	return (0);
 }
