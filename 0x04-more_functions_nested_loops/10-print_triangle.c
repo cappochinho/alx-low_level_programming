72e4970a6029d73caf1440a851374389fbb6dcb6
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-09 22:14:25 +0000

    Fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,29 @@
 #include "main.h"
 
 /**
- * print_triangle - Description below
- * Description: Prints a right-angled triangle
- * @size: Integer input
- * Return: No value
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: An input integer
+ * Return: Always 0
  */
-
 void print_triangle(int size)
 {
-	int i;
-	int j;
+	int i = 0, j, n = size - 1;
 
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		for (; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 0; j < size; j++)
 			{
-				if (j >= (size - i + 1) && j <= size)
-					_putchar(35);
-				else
+				if (j < n)
 					_putchar(' ');
+				else
+					_putchar('#');
 			}
+			n--;
 			_putchar('\n');
 		}
 	}
-
 	else
 		_putchar('\n');
 }
