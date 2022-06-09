23c583c30cba4808850acf21b8bb829c9d8424f9
cappochinho <edwardprempeh18@gmail.com>
2022-06-09 02:14:49 -0700

    Is this Uppercase?

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isupper - Described below
+ * Description: Checks if a character constant is uppercase
+ * @c: Character input
+ * Return: 1 if TRUE or 0 if FALSE
+ */
+
+int _isupper(int c)
+{
+	int upper;
+
+	upper = 0;
+	for (c >= 'A'; c <= 'Z'; c++)
+	{
+		if (c != EOF)
+		{
+			upper = 1;
+		}
+		else
+		{
+			upper = 0;
+		}
+	}
+	return (upper);
+}
