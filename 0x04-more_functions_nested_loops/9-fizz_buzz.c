abc0944800533b56c4b4859ffeeb206b17bfb73d
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-09 22:12:23 +0000

    Fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,26 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Description below
- * Cofirms if a multiple of 3 or 5 or both with a word output
- *
- * Return: Always 0(Program success)
+ * main - Entry piont
+ * Return: Always 0
  */
-
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	i = 1;
-
-	for (; i <= 100; i++)
+	for (; i < 100 ; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz");
-		else if ((i % 5) == 0)
-			printf("Buzz");
-		else if ((i % 3) == 0)
-			printf("Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
-			printf("%i", i);
-		putchar(' ');
+			printf("%d ", i);
 	}
-	putchar('\n');
-	
+	printf("Buzz\n");
+
 	return (0);
 }
