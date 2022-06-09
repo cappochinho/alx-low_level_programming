eeb959af5e118d4e5b6f276f4d196f4b11658c3d
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-09 22:10:22 +0000

    Fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,17 +16,17 @@ int main(void)
 
 	for (; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
-			printf("Fizz");
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
 		else if ((i % 5) == 0)
 			printf("Buzz");
-		else if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
 		else
 			printf("%i", i);
-		putchar(' ');
+		_putchar(' ');
 	}
-	printf('\n');
-
+	_putchar('\n');
+	
 	return (0);
 }
