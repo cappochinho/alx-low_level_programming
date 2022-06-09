f4f047d75eb8aa8ff1e930e8ae107d5c8d880463
cappochinho <edwardprempeh18@gmail.com>
2022-06-09 13:22:06 -0700

    FizzBuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Description below
+ * Cofirms if a multiple of 3 or 5 or both with a word output
+ *
+ * Return: Always 0(Program success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else
+			printf("%i", i);
+		putchar(' ');
+	}
+
+	return (0);
+}
