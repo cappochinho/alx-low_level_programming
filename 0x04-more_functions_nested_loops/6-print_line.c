841f7ffd120da26eb61f8d43e86c721c535dc358
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-09 21:56:50 +0000

    Fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,14 @@
  * @n: Integer input
  * Return: No value
  */
-
 void print_line(int n)
 {
 	int i = 0;
-	
+
 	if (n > 0)
 	{
-		while (i < n)
-		{
-			_putchar(95);
-			i++;
-		}
+		for (; i < n; i++)
+			_putchar('_');
 	}
 	_putchar('\n');
 }
