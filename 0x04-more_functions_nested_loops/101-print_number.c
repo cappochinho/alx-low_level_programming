9dcb46e8179aa4fbbd890486967447bf202f8550
cappochinho <edwardprempeh18@gmail.com>
2022-06-11 05:54:38 -0700

    Print numbers using only _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,12 +9,14 @@
 
 void print_number(int n)
 {
+	unsigned a = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		a = -a;
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar('0' + n % 10);
+	if ((a / 10) > 0)
+		print_number(a / 10);
+	_putchar('0' + a % 10);
 }
