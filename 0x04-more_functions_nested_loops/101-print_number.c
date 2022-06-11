85801022aa0417590c720b872fcb22b0cab226ba
cappochinho <edwardprempeh18@gmail.com>
2022-06-11 05:34:24 -0700

    Print numbers using only _putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Described below
+ * Description: Prints any number it is given using only _putchar
+ * @n: Integer input
+ * Return: No value
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n == 0)
+		_putchar(n);
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
