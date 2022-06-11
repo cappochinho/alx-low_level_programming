c9ee7d2cae6299e2bb6ad6f0a22f3e5b00741fad
cappochinho <edwardprempeh18@gmail.com>
2022-06-11 05:47:18 -0700

    Print numbers using only _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,10 +14,7 @@ void print_number(int n)
 		_putchar('-');
 		n = n * -1;
 	}
-
-	if (n == 0)
-		_putchar(n);
-	if (n / 10)
+	if ((n / 10) > 0)
 		print_number(n / 10);
 	_putchar('0' + n % 10);
 }
