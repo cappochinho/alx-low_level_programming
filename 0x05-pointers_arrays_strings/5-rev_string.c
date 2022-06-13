91f1f3d8c5351a665e76b82bf11f8866b60da541
cappochinho <edwardprempeh18@gmail.com>
2022-06-13 13:04:17 -0700

    Reverse a string using pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Described below
+ * Description: Takes a string a reverses it
+ * @s: String input
+ * Return: No value
+ */
+
+void rev_string(char *s)
+{
+	int len, i;
+	char *s_start, *s_end, ch;
+
+	len = strlen(s);
+	s_start = s;
+	s_end = s;
+	for (i = 0; (i < len - 1); i++)
+		s_end++;
+	for (i = 0; i < len / 2; i++)
+	{
+		ch = *s_end;
+		*s_end = *s_start;
+		*s_start = ch;
+
+		s_start++;
+		s_end--;
+	}
+}
