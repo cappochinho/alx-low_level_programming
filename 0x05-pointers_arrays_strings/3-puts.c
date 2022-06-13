4a1bb7b0dde7ea629d1fa0dc8353490b4f2e573b
cappochinho <edwardprempeh18@gmail.com>
2022-06-13 11:34:58 -0700

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Described below
+ * Description: Takes a string literal and prints it
+ * @str: String input
+ * Return: No value
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	printf("\n");
+}
