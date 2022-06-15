78987e3e9bc8bc640d820af0221413d85ef5ecbd
cappochinho <edwardprempeh18@gmail.com>
2022-06-15 07:46:59 -0700

    Strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - Described below
+ * Description: Takes a specified length of a string
+ * and copies it into another string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of characters to be copied
+ * Return: Destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
