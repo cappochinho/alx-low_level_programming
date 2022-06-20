fd212f73fc91e144a3c76ec0d8807c3bd1e3036c
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-20 22:28:03 +0000

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
