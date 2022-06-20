cee0086e72a76c7e7336c2545110ec02c81df91c
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-20 22:27:15 +0000

    # Memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - Described below
+ * Description: Copies n bytes from src to dest
+ * @dest: Destination
+ * @src: Source
+ * @n: Number of bytes to be copied
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
