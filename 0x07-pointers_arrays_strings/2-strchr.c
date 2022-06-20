ece51ccdb9e5543a6cd7df8f286cc616e29851aa
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-20 23:22:19 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-    	for (; s[i] != '\0'; i++)
+    	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (c == s[i])
 			break;
 	}
-	
+
 	if (c == s[i])
 		return (s + i);
 
