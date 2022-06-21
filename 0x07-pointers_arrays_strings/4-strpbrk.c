5edb4dcdbd3192323ed75ee9e0189b2d1fcadabf
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-21 00:49:42 +0000

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,50 @@
 #include "main.h"
+#include <stddef.h>
+
+int in_accept(char c, char *accept);
 
 /**
- * _strpbrk - Checks for a string occurence
- * @s: String to be checked
- * @accept: Checking string
- * Return: Pointer to first occurence
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to be checked
+ * @accept: string containing the only accepted bytes
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	unsigned int i;
+	char *first = NULL;
 
-    	for (i = 0; s[i] != '\0'; i++)
-    	{
-        	for (j = 0; accept[j] != '\0'; j++)
-        	{
-            		if (s[i] == accept[j])
-                		return (s + i);
-        	}
-    	}
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (in_accept(s[i], accept))
+		{
+			first = (s + i);
+			break;
+		}
+	}
 
-    	if (s[i] == accept[j])
-        	return (s + i);
+	return (first);
+}
 
-    	return (NULL);
+
+/**
+ * in_accept - checks if agiven character is in the string accept
+ * @c: character to be checked
+ * @accept: string containingthe only accepted characters
+ *
+ * Return: 1 if c is in accept. Otherwise 0
+ */
+int in_accept(char c, char *accept)
+{
+	int i;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (c == accept[i])
+			return (1);
+	}
+
+	return (0);
 }
