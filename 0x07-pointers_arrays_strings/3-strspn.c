1e33057fa09ef74199db950c2ee9d3763b8ac6a4
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-21 00:32:04 +0000

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,33 +1,47 @@
 #include "main.h"
 
-/**
- * _strspn - Described below
- * Description: Checks the length of accept covered in s
- * 
- * @s: Main string
- * @accept: Substring
- * Return: Length covered by substring
- */
+int in_accept(char c, char *accept);
 
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to check
+ * @accept: string containing the only accepted characters
+ *
+ * Return: the number of bytes in the initial segment of s which consist
+ * only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, hold = 0;
-    	int s_len;
+	unsigned int length = 0;
+	unsigned int i;
 
-    	for (i = 0; (s[i] != '\0') && (s[i] != ' '); i++)
-        	s_len = i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (in_accept(s[i], accept))
+			length++;
+		else
+			break;
+	}
 
-    	for (j = 0; j < s_len; j++)
-    	{
-        	for (i = 0; i < s_len; i++)
-        	{
-            		if (accept[j] == s[i])
-            		{
-                		if (i > hold)
-                    			hold = i;
-            		}
-        	}
-    	}
+	return (length);
+}
 
-    	return (hold + 1);
+/**
+ * in_accept - checks if agiven character is in the string accept
+ * @c: character to be checked
+ * @accept: string containingthe only accepted characters
+ *
+ * Return: 1 if c is in accept. Otherwise 0
+ */
+int in_accept(char c, char *accept)
+{
+	int i;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (c == accept[i])
+			return (1);
+	}
+
+	return (0);
 }
