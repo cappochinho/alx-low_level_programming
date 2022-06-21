366e489a4b60bd5a56972df328a24388dff25255
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-21 00:22:51 +0000

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - Described below
+ * Description: Checks the length of accept covered in s
+ * 
+ * @s: Main string
+ * @accept: Substring
+ * Return: Length covered by substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int i, j, hold = 0;
+    int s_len;
+
+    for (i = 0; (s[i] != '\0') && (s[i] != ' '); i++)
+        s_len = i;
+
+    for (j = 0; j < s_len; j++)
+    {
+        for (i = 0; i < s_len; i++)
+        {
+            if (accept[j] == s[i])
+            {
+                if (i > hold)
+                    hold = i;
+            }
+        }
+    }
+
+    return (hold + 1);
+}
