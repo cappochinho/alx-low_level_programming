265e6cbb74b4501c0cb98486f3ee817b1b8e454f
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-27 11:15:33 +0000

    Create array with character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,12 @@
 
 char *create_array(unsigned int size, char c)
 {
+        char *a;
+
+        unsigned int i = 0;
+
         if (size == 0)
                 return (NULL);
-
-        char *a;
-        unsigned int i = 0;
 
         a = malloc(size * sizeof(char));
 
