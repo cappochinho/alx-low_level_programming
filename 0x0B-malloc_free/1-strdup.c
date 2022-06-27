f72ca0bcac6d1efceba5f088a0c9585eb42b707b
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-06-27 13:06:40 +0000

    Malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,11 +11,15 @@ char *_strdup(char *str)
 	char *a;
 	unsigned int len, i = 0;
 
+	if (str == 0)
+		return (NULL);
 	while (str[i])
 		i++;
 	len = i + 1;
 
 	a = (char *) malloc(len * sizeof(char));
+	if (a == 0)
+		return (NULL);
 
 	for (i = 0; i < len; i++)
 		*(a + i) = *(str + i);
