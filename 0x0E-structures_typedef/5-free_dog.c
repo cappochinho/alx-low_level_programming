dc69d8475dc38f92ea2632d9424bdca783b360fb
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-07-04 18:21:23 +0000

    Create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - Free dog structure
+ * @d: pointer to struct to be freed
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
