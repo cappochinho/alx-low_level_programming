8deb0e7c46c11a8cbe9828b936e661aaeb3ae5e3
cappochinho <edwardprempeh18@gmail.com>
2022-07-04 05:12:58 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Check next line
+ * Description: Prints the name of this file
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
