1733ce1fff0b6a980906587c3fd16c391c8d9938
cappochinho <edwardprempeh18@gmail.com>
2022-07-06 14:01:52 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints the name passed to the function
+ * @name: Pointer to a string literal
+ * @f: Function pointer
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+#include <stdio.h>
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTER_H */
