bf8739464bf4d5c2e0621619f1e6cfd2c8916a14
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-07-06 23:03:35 +0000

    Class 1

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * main - Program for simple operations
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: exits 0 on normal execution, 98 on argc error, 99 on illegal operator
+ */
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
