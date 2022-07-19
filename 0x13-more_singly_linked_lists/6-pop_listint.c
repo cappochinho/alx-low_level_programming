bad32afad89dbf8a384d5cdc12be7431c2638953
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-07-19 19:46:42 +0000

    Pop from stack

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,25 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a linked list
- * @head: head node
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: head of linked list
  * Return: data contained in head node
  */
 
 int pop_listint(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *current;
 	int d;
 
 	if (head == NULL)
 		return (0);
 
-	tmp = *head;
-	d = tmp->n;
-	(*head) = tmp->next;
-	free(tmp);
+	current = *head;
+	if (current == NULL)
+		return (0);
 
+	d = current->n;
+	*head = current->next;
+	free(current);
 	return (d);
 }
